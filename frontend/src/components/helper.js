2067174974a2cbc0c64f6ec42eb22c6c2be25741
github_KGan31 <kavan.gandhi31@gmail.com>
2023-08-25 11:27:35 +0530

    Adding GIF

--- frontend/src/components/helper.js
+++ frontend/src/components/helper.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://best-buy-backend.onrender.com";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://best-buy-backend.onrender.com";
+// export const BASE_URL = "http://localhost:8000";
     
